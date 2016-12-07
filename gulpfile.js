fb837c5bb82c5895a2b808b654875db3a5b21430
kovarnyirus <kovarnyi@gmail.com>
2016-12-07 21:19:47 +0300

    gulp instal

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var browserSync = require('browser-sync');
 
 gulp.task('less', function() {
-  gulp.src("less/style.less")
+   return gulp.src('app/less/*.less')
     .pipe(less())
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.reload({stream: true}));
 });
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    server:{
+      baseDir: 'app'
+    },
+    notify:false
+  });
+});
+
+gulp.task('watch', ['browser-sync', 'less'], function(){
+  gulp.watch('app/less/*.less',['less']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/*.js', browserSync.reload);
+})
