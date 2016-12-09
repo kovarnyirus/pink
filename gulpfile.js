d7ace2195c65d209f63ca37ce6d4c7260f28ebfd
kovarnyirus <kovarnyi@gmail.com>
2016-12-09 10:55:08 +0300

    work

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 
-gulp.task('less', function() {
-   return gulp.src('app/less/*.less')
+
+gulp.task('style', function() {
+  return gulp.src('app/less/style.less')
+    .pipe(plumber())
     .pipe(less())
+    .pipe(autoprefixer({browsers: [
+        "last 2 versions"
+          ]}))
     .pipe(gulp.dest("app/css"))
     .pipe(browserSync.reload({stream: true}));
 });
+
+gulp.task('css-libs', ['style'], function(){
+  return gulp.src('app/css/style.css')
+  .pipe(cssnano())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('app/css'));
+})
 
 gulp.task('browser-sync', function(){
   browserSync({
@@ -18,8 +35,10 @@ gulp.task('browser-sync', function(){
   });
 });
 
-gulp.task('watch', ['browser-sync', 'less'], function(){
-  gulp.watch('app/less/*.less',['less']);
+gulp.task('watch', ['browser-sync', 'css-libs'], function(){
+  gulp.watch('app/less/**/*.less', ['style']);
+  gulp.watch('app/css/*.css', ['css-libs']);
+  gulp.watch('app/css/*.css', browserSync.reload);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/*.js', browserSync.reload);
 })
